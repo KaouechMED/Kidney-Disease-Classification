8461c017d637aa5d021269bacd1cf928bca68fed
Mohamed Kaouech <123780902+KaouechMED@users.noreply.github.com>
2024-02-04 11:41:38 +0100

    model path updated

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -73,7 +73,7 @@ class ConfigurationManager:
     
     def get_evaluation_config(self) -> EvaluationConfig:
         eval_config = EvaluationConfig(
-            path_of_model="artifacts/training/model.h5",
+            path_of_model="model/model.h5",
             training_data="artifacts/data_ingestion/kidney-ct-scan-image",
             mlflow_uri="https://dagshub.com/KaouechMED/Kidney-Disease-Classification.mlflow",
             all_params=self.params,
